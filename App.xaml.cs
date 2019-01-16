b5654b28adbd4a83cb85b78b00ee0b826bdd01de
Peter Amrehn <peter.wendorff@gmx.de>
2019-01-16 22:57:32 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfCore3Experiments
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
